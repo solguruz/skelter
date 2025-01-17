b2bd2ff7713a61deafbac1b70ae18f8ea52bb6c8
Mihir Joshi <mihirj@solguruz.com>
2025-01-17 17:20:17 +0530

    ref: add platform running method

--- test/flutter_test_config.dart
+++ test/flutter_test_config.dart
@@ -5,10 +5,12 @@ import 'package:flutter_skeleton/widgets/styling/app_theme_data.dart';
 
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
   const isRunningInCi = bool.fromEnvironment('CI');
-
   return AlchemistConfig.runWithConfig(
     config: AlchemistConfig(
       theme: AppThemesData.themeData[AppThemeEnum.LightTheme],
+      platformGoldensConfig: const PlatformGoldensConfig(
+        enabled: !isRunningInCi,
+      ),
       ciGoldensConfig: const CiGoldensConfig(
         enabled: isRunningInCi,
       ),
