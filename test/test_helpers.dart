7a6b41713902480a250174ac2811a5bf5b041068
Mihir Joshi <mihirj@solguruz.com>
2025-01-17 18:07:01 +0530

    ref: add script for generating linux images

--- test/test_helpers.dart
+++ test/test_helpers.dart
@@ -1,8 +1,8 @@
 import 'package:alchemist/alchemist.dart';
-import 'package:flutter_skeleton/i18n/localization.dart';
-import 'package:flutter_skeleton/widgets/styling/app_theme_data.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_skeleton/i18n/localization.dart';
+import 'package:flutter_skeleton/widgets/styling/app_theme_data.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 extension WidgetTestHelper on WidgetTester {
