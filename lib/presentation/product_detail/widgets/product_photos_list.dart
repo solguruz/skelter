f1d7b4416c114eb7ccf2ce3c43fd01fabbbc7bc7
Ganesh Shinde <ganeshs@solguruz.com>
2025-10-16 17:15:52 +0530

    refactor: remove auto scroll feature

--- lib/presentation/product_detail/widgets/product_photos_list.dart
+++ lib/presentation/product_detail/widgets/product_photos_list.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
@@ -7,7 +5,7 @@ import 'package:skelter/gen/assets.gen.dart';
 import 'package:skelter/utils/app_environment.dart';
 import 'package:skelter/widgets/styling/app_colors.dart';
 
-class ProductPhotosList extends StatefulWidget {
+class ProductPhotosList extends StatelessWidget {
   final List<String> photos;
   final int selectedImageIndex;
   final ValueChanged<int> onImageChanged;
@@ -20,63 +18,24 @@ class ProductPhotosList extends StatefulWidget {
   });
 
   @override
-  State<ProductPhotosList> createState() => _ProductPhotosListState();
-}
-
-class _ProductPhotosListState extends State<ProductPhotosList> {
-  late final ScrollController _scrollController;
-  Timer? _autoScrollTimer;
-  final double _itemWidth = 100;
-  final double _itemSpacing = 10;
-
-  @override
-  void initState() {
-    super.initState();
-    _scrollController = ScrollController();
-    _startAutoScroll();
-  }
-
-  void _startAutoScroll() {
-    _autoScrollTimer = Timer.periodic(const Duration(seconds: 5), (_) {
-      if (!mounted) return;
-      final totalItems = widget.photos.length;
-      final nextIndex = (widget.selectedImageIndex + 1) % totalItems;
-      widget.onImageChanged(nextIndex);
-      _scrollController.animateTo(
-        nextIndex * (_itemWidth + _itemSpacing),
-        duration: const Duration(milliseconds: 400),
-        curve: Curves.easeInOut,
-      );
-    });
-  }
-
-  @override
-  void dispose() {
-    _autoScrollTimer?.cancel();
-    _scrollController.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     final isFromTestEnvironment = AppEnvironment.isTestEnvironment;
 
     return SizedBox(
       height: 100,
       child: ListView.separated(
-        controller: _scrollController,
         scrollDirection: Axis.horizontal,
-        itemCount: widget.photos.length,
+        itemCount: photos.length,
         physics: const ClampingScrollPhysics(),
         separatorBuilder: (_, __) => const SizedBox(width: 10),
         itemBuilder: (context, index) {
-          final isImageSelected = index == widget.selectedImageIndex;
-          final productPhotosUrl = widget.photos[index];
+          final isImageSelected = index == selectedImageIndex;
+          final productPhotosUrl = photos[index];
 
           return GestureDetector(
-            onTap: () => widget.onImageChanged(index),
+            onTap: () => onImageChanged(index),
             child: Container(
-              width: _itemWidth,
+              width: 100,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(12),
                 border: Border.all(
